e28dbc74c645d7c13245e69760a456415e00a39a
toStoying <707172846@qq.com>
2018-12-28 22:17:37 +0800

    test

--- weChatSave/pages/index/index.js
+++ weChatSave/pages/index/index.js
@@ -3,7 +3,7 @@
 const app = getApp()
 
 const pageData = {}
-for (let i = 1; i < 5; i++) {
+for (let i = 1; i < 4; i++) {
   (function (index) {
     pageData['slider' + index + 'change'] = function (e) {
       console.log('slider' + index + '发生 change 事件，携带值为', e.detail.value)
