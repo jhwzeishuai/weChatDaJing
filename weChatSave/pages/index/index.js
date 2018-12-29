20a7934ac337baef79e7c32f4868bd6dfd0ca150
toStoying <707172846@qq.com>
2018-12-29 14:20:03 +0800

    Revert "test"
    This reverts commit 37b3e876119218372215cccc6309c76c3d8a7df1.

--- weChatSave/pages/index/index.js
+++ weChatSave/pages/index/index.js
@@ -6,7 +6,7 @@ const pageData = {}
 for (let i = 1; i < 4; i++) {
   (function (index) {
     pageData['slider' + index + 'change'] = function (e) {
-      console.log('slider' + index + '发生 change 事件，dfdf携带值为', e.detail.value)
+      console.log('slider' + index + '发生 change 事件，携带值为', e.detail.value)
     }
   }(i))
 }
